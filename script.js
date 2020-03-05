0c2f34a76a4e2a5a5f39e3f26d810c0c1f640d3d
Nathan Long <nrlong13@gmail.com>
2020-03-04 20:28:18 -0500

    updated JS search button to return information to log

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-//build weather information
+$(document).ready(function () {
 
-const key= "198e7cd123c38028748d31ffb347ffa7";
-// const weatherQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ search +"&appid=" + keyWeather;
+    //build weather information
 
-$(".btn").click(function () {
-    let search = $(this).prev().val()
-    const weatherQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ search +"&appid=" + key;
+    const key = "198e7cd123c38028748d31ffb347ffa7";
 
-$.ajax({
-    url: weatherQueryURL,
-    method: "GET"
-})
-.then(function(responseWeather){
+    $(".searchBtn").click(function () {
+        let search = $(".form-control").val()
 
-    console.log(responseWeather);
-});
-});
+        const weatherQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + key;
+        $.ajax({
+                url: weatherQueryURL,
+                method: "GET"
+        })
+            .then(function (responseWeather) {
 
-//build local events information
-const localQueryURL = "https://www.triposo.com/api/20190906/location.json?id=Denver&account=U9R2XMW3&token=" + key;
 
-$.ajax({
-    url: localQueryURL,
-    method: "GET"
-})
-.then(function(responseLocal){
+                console.log(responseWeather);
+            });             
 
-    console.log(responseLocal);
+        //build local events information
+        const localKey = "9zmr4wz97r5l4jenh85ap1enclfnccwf"
+        const localQueryURL = "https://www.triposo.com/api/20190906/location.json?id=" + search + "&account=U9R2XMW3&token=" + localKey;
+
+        $.ajax({
+                url: localQueryURL,
+                method: "GET"
+            })
+            .then(function (responseLocal) {
+
+                let responseInfo = responseLocal.results[0];
+
+                let cityName = responseInfo.id;
+                let cityParent = responseInfo.parent_id;
+                let cityCountry = responseInfo.country_id;
+                let citySnip = responseInfo.snippet;
+
+                console.log(citySnip);
+                console.log(cityCountry);
+                console.log(cityParent)
+                console.log(cityName);
+
+                console.log(responseLocal);
+            })
+
+    });
+
 })
